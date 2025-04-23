0443d46f7ec4c0c85f1a7f893a2afe0e2f1f3937
Simon Bachhuber <Simon.Bachhuberr@gmail.com>
2025-04-23 12:16:01 +0200

    v0.2.0; adds new constraint to `imt.methods.HeadCor`, requires `qmt` >= 0.3.0

--- src/imt/methods/_qmt.py
+++ src/imt/methods/_qmt.py
@@ -2,10 +2,13 @@
 Heading correction methods from https://github.com/dlaidig/qmt
 """
 
+from typing import Optional
+
 import numpy as np
 import qmt
 
 from .._base import Method
+from ..wrappers._jpos import _compute_j1_j2
 
 
 class HeadCor(Method):
@@ -13,25 +16,21 @@ class HeadCor(Method):
         self,
         dof: int,
         axes_directions: np.ndarray | None,
-        method_1d: str = "1d_corr",
-        method_2d: str = "euler",
         use_mag_only_if_both: bool = False,
         offline_vqf: bool = False,
+        heading_correction_estSettings: Optional[dict] = None,
     ):
         """Uses `VQF` + `qmt.headingCorrection`
 
         Args:
             dof (int): The DOF of the joint between two segments
             axes_directions (np.ndarray | None): The joint axes directions of this joint
-            method_1d (str, optional): Method to use for 1D joint. Defaults to "1d_corr"
-            method_2d (str, optional): Method to use for 2D joint. Defaults to "euler".
             use_mag_only_if_both (bool, optional): Use magnetometer measurements for VQF
                 only if both IMUs have magnetometer measurements. Defaults to False.
             offline_vqf (bool, optional): If `True` uses offlineVQF else onlineVQF.
                 Defaults to False.
-
-        Raises:
-            Exception: If `dof` == 3
+            heading_correction_estSettings(dict, optional): `estSettings` dictionary
+                that is forward to `qmt.headingCorrection`; refer to docstring there
         """
         self.strict = use_mag_only_if_both
         self.offline_vqf = offline_vqf
@@ -45,16 +44,21 @@ class HeadCor(Method):
         else:
             self.axes_directions = None
 
-        assert dof in [
-            1,
-            2,
-        ], "Currently only 1D or 2D joints supported with this method"
         self.dof = dof
+        assert dof in [1, 2, 3]
 
         if dof == 2 and axes_directions is None:
             raise Exception("For 2D joints this methods needs joint axes information")
 
-        self.method_1d, self.method_2d = method_1d, method_2d
+        default_constraint = {1: "1d_corr", 2: "euler", 3: "conn"}
+        self._heading_correction_estSettings = {
+            # set default constraint depending on `dof`
+            "constraint": default_constraint[dof]
+        }
+        if heading_correction_estSettings is not None:
+            self._heading_correction_estSettings.update(
+                heading_correction_estSettings.copy()
+            )
 
     def apply(self, acc1, acc2, gyr1, gyr2, mag1, mag2, T: int | None):
         if T is None:
@@ -74,6 +78,11 @@ class HeadCor(Method):
         q2 = _vqf(gyr2, acc2, mag2, params=dict(Ts=Ts))
 
         ts = np.linspace(0, (T - 1) * Ts, T)
+
+        estSettings = self._heading_correction_estSettings.copy()
+        if "windowTime" not in estSettings:
+            estSettings["windowTime"] = min(8.0, ts[-1])
+
         if self.dof == 1:
             if self.axes_directions is None:
                 axis_imu1, axis_imu2 = qmt.jointAxisEstHingeOlsson(
@@ -90,28 +99,47 @@ class HeadCor(Method):
             q2 = qmt.headingCorrection(
                 gyr1,
                 gyr2,
+                acc1,
+                acc2,
                 q1,
                 q2,
                 ts,
                 axis,
                 None,
-                estSettings=dict(
-                    constraint=self.method_1d, windowTime=min(8.0, ts[-1])
-                ),
+                estSettings=estSettings,
             )[0]
-        else:
+        elif self.dof == 2:
             q2 = qmt.headingCorrection(
                 gyr1,
                 gyr2,
+                acc1,
+                acc2,
                 q1,
                 q2,
                 ts,
                 self.axes_directions,
                 None,
-                estSettings=dict(
-                    constraint=self.method_2d, windowTime=min(8.0, ts[-1])
-                ),
+                estSettings=estSettings,
             )[0]
+        elif self.dof == 3:
+            r1, r2 = _compute_j1_j2(Ts, False, False, acc1, acc2, gyr1, gyr2)
+            q2 = qmt.headingCorrection(
+                gyr1,
+                gyr2,
+                acc1,
+                acc2,
+                q1,
+                q2,
+                ts,
+                # we only do this such that internally `headingCorrection` sets `dof`
+                # to 3; only the shape of the array is used in this case
+                np.ones((3, 3)),
+                {"r1": -r1, "r2": -r2},  # minus to convert from jc-to-IMU to IMU-to-jc
+                estSettings=estSettings,
+            )[0]
+        else:
+            raise NotImplementedError
+
         return qmt.qmult(qmt.qinv(q1), q2), {}
 
     def _process_mag(self, *mags):
